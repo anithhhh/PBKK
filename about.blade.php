5aa98cd97d741baf0e67ae4adc8279ddfe94525c
anithhhh <anisztalib@gmail.com>
2024-09-25 12:47:36 +0800

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl"> Halaman About</h3>
+  <p>Nama: {{ $name }}</p>
+  <p>welcome to my page.  I’m an international exchange student from Universiti Malaysia Pahang! It's nice to see you guys here! I’m excited to share more about myself and my experiences</p>
+
+  <img src="img/depanump.jpg" alt="Menara Kuantan 188" width="200">
+      <figcaption>This is a picture of me and my friends! </figcaption>
+
+</x-layout>
+
+
