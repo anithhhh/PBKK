1a2d848aae869c50f971358fb3ed03d085226db2
anithhhh <anisztalib@gmail.com>
2024-10-01 13:35:00 +0800

    Add files via upload

--- contact.blade.php
+++ contact.blade.php
@@ -1,5 +1,20 @@
 <x-layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
-  <h3 class="text-xl"> Halaman Contact</h3>
+    <x-slot name="title">{{ $title }}</x-slot>
+    <h3 class="text-xl">Halaman Contact</h3>
+
+    <p>You can follow me on my social media channels:</p>
+    
+    <ul>
+        <li>
+            <a href="https://www.instagram.com/anistalib_" target="_blank" class="text-blue-600 underline">
+                Instagram
+            </a>
+        </li>
+        <li>
+            <a href="https://www.youtube.com/@anistalib5948" target="_blank" class="text-black-600 underline">
+                YouTube
+            </a>
+        </li>
+    </ul>
 
 </x-layout>
